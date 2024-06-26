aa83580f494e034037e51d6f9d1671b9272f6205
Huiping27 <cathylhp@gmail.com>
2024-06-26 10:16:02 +0200

    java test

---
+++ Example.java
@@ -0,0 +1,10 @@
+// SimpleExample.java
+public class Example {
+    public static void main(String[] args) {
+        // Print a simple message
+        System.out.println("Hello, This is Java!\nHow can you do this");
+    }
+}
+
+
+ 
---
+++ Example2.java
@@ -0,0 +1,10 @@
+public class Example2 {public static void main(String args []) { 
+    int num; // this declares a varaibale called num
+    num=100;//this assigns num the valze 100
+    System.out.println("This is num:"+num);
+    num=num*2;
+    System.out.print("The value of num*2 is ");
+    System.out.println(num);
+}
+    
+}
